a27518db5874cee04755d6235213d91014f00bef
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-12-07 12:55:21 +0530

    XAMARIN-34117 - Sample and screenshot added.

---
+++ ExpanderXamarin/ExpanderXamarin/ExpanderXamarin/Views/ExpandableListView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace ExpanderXamarin
+{
+    [Preserve(AllMembers = true)]
+    public partial class ExpandableListView : ContentPage
+    {
+		public ExpandableListView()
+		{
+			InitializeComponent();
+       	}
+	}
+}
